1006aaaf0f3d640c8e1e88acdf8bb4d1ec609672
Beatriz Mandim <beatrizjuliomandim@gmail.com>
2023-10-07 17:08:32 -0300

    ajustes

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,13 +1,12 @@
 //LOCAL ONDE OCORRE A COMUNICAÇÃO DAS APIS (REGISTRO, SENHA E QUALQUER OUTRA ROTA)
 
-
-import api from "@/services/api";
-import { ReactNode, createContext } from "react";
-import { AxiosResponse } from "axios";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { destroyCookie, parseCookies, setCookie } from "nookies";
-import { ReactNode, createContext, useEffect, useState } from "react";
-import { toast } from "react-toastify";
+// import api from "@/services/api";
+import { ReactNode, createContext, useState } from "react";
+// import { AxiosResponse } from "axios";
+// import jwt, { JwtPayload } from "jsonwebtoken";
+// import { destroyCookie, parseCookies, setCookie } from "nookies";
+// import { ReactNode, createContext, useEffect, useState } from "react";
+// import { toast } from "react-toastify";
 
 interface IUser {
   user_email: string;
@@ -21,41 +20,43 @@ interface IAuthProvider {
   children: ReactNode;
 }
 
-interface IAuthContext {
-    user: IUser | null;
-    signOut: () => Promise<void>;
-    signOutResident: () => Promise<void>;
-    currentBranch: ICurrentBranch | null;
-    getCurrentBranch: () => Promise<void>;
-    getProductPermissions: () => Promise<void>;
-    userBranches: IBranch[] | null;
-    getProfile: (userToken: string) => Promise<void>;
-    decodedToken: JwtPayload | null;
-    productDashboard: any;
-}
+// interface IAuthContext {
+//     user: IUser | null;
+//     signOut: () => Promise<void>;
+//     signOutResident: () => Promise<void>;
+//     currentBranch: ICurrentBranch | null;
+//     getCurrentBranch: () => Promise<void>;
+//     getProductPermissions: () => Promise<void>;
+//     userBranches: IBranch[] | null;
+//     getProfile: (userToken: string) => Promise<void>;
+//     decodedToken: JwtPayload | null;
+//     productDashboard: any;
+// }
 
-export const AuthContext = createContext({} as IAuthContext);
+//export const AuthContext = createContext({} as IAuthContext);
 
 const AuthProvider = ({ children }: IAuthProvider) => {
+  // const [user, setUser] = useState<IUser>({});
   return (
-    <AuthContext.Provider
-      value={
-        {
-            user,
-            getProductPermissions,
-            signOut,
-            userBranches,
-            currentBranch,
-            getCurrentBranch,
-            getProfile,
-            decodedToken,
-            signOutResident,
-            productDashboard,
-        }
-      }
-    >
-      {children}
-    </AuthContext.Provider>
+    console.log("OI")
+    // <AuthContext.Provider
+    //   value={
+    //     {
+    //         user,
+    //         getProductPermissions,
+    //         signOut,
+    //         userBranches,
+    //         currentBranch,
+    //         getCurrentBranch,
+    //         getProfile,
+    //         decodedToken,
+    //         signOutResident,
+    //         productDashboard,
+    //     }
+    //   }
+    // >
+    //   {children}
+    // </AuthContext.Provider>
   );
 };
 
