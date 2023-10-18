16eb45018cccdee85598084b964f0348b5389360
Beatriz Mandim <beatrizjuliomandim@gmail.com>
2023-10-18 13:20:56 -0300

    finalizado o desing do aprovador e motorista

--- src/pages/approver/fichaRetorno/components/ReturnSheetsList.tsx
+++ src/pages/approver/fichaRetorno/components/ReturnSheetsList.tsx
@@ -1,36 +1,69 @@
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import List from "@mui/material/List";
 
 const ReturnSheetsList = () => {
+  const lista = [
+    {
+      id: 1,
+      nome: "Doblo",
+      status: "Disponível",
+    },
+    {
+      id: 2,
+      nome: "Doblo2",
+      status: "Disponível",
+    },
+  ];
+
   return (
-    <List
-      sx={{
-        width: '100%',
-        maxWidth: 360,
-        bgcolor: 'background.paper',
-        position: 'relative',
-        overflow: 'auto',
-        maxHeight: 300,
-        '& ul': { padding: 0 },
-      }}
-      subheader={<li />}
-    >
-      {[0, 1, 2, 3, 4].map((sectionId) => (
-        <li key={`section-${sectionId}`}>
-          <ul>
-            <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-            {[0, 1, 2].map((item) => (
-              <ListItem key={`item-${sectionId}-${item}`}>
-                <ListItemText primary={`Item ${item}`} />
-              </ListItem>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </List>
+    <>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          position: "relative",
+          overflow: "auto",
+          maxHeight: 300,
+          "& ul": { padding: 0 },
+        }}
+        subheader={<li />}
+      >
+        {lista.map((carro: any) => (
+          <li key={`section-${carro.id}`}>
+            <ul>
+              <Card sx={{ maxWidth: 330, marginBottom: 5, marginTop: 2 }}>
+                <CardMedia
+                  sx={{ height: 140 }}
+                  image="https://s2-autoesporte.glbimg.com/OkOTAbm8c0hcBorNJK_n5VVN-3g=/0x0:1200x875/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2021/6/a/sh5QGuQUWsns9zfrqYGQ/fiat-doblo-50.jpg"
+                  title="carro"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {carro.nome}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {carro.status}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Aprovar</Button>
+                  {/* <Button size="small">Solicitar</Button> */}
+                </CardActions>
+              </Card>
+            </ul>
+          </li>
+        ))}
+      </List>
+    </>
   );
-}
+};
 
 export default ReturnSheetsList;
