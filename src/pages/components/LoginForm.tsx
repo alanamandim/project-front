343827ed21121fef15ba7b3c7c6f6c2e0e826174
Beatriz Mandim <beatrizjuliomandim@gmail.com>
2023-10-26 21:29:59 -0300

    teste4

--- src/pages/components/LoginForm.tsx
+++ src/pages/components/LoginForm.tsx
@@ -44,7 +44,7 @@ const LoginForm = () => {
   }
 
   async function submitLogin() {
-    const newUserData: FormValues = {
+    const newUserData = {
       email: email,
       senha: senha,
     };
