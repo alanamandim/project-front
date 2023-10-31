a4ea723199aadbf582bf8008ac71b63bc80548d9
Beatriz Mandim <beatrizjuliomandim@gmail.com>
2023-10-31 16:05:45 -0300

    teste

--- src/pages/driver/fichaInspecao/components/FormInspecao.tsx
+++ src/pages/driver/fichaInspecao/components/FormInspecao.tsx
@@ -93,7 +93,7 @@ const InspecaoForm: any = () => {
           progress: undefined,
         });
         localStorage.removeItem("idSolicitacao");
-      } else if (response.status == 500) {
+      } else if (response.status == 400) {
         toast.error(
           `Não pode fazer inspeção sem aprovação da solicitação: ${response.status}`,
           {
--- src/pages/driver/reservasVeiculo/components/ReservasVeiculo.tsx
+++ src/pages/driver/reservasVeiculo/components/ReservasVeiculo.tsx
@@ -7,26 +7,42 @@ import { AuthContext } from "../../../../context/AuthContext";
 const ReservasForm: any = async () => {
   const url = "http://localhost:8080";
   const userContext = useContext(AuthContext);
-
-  const [dataGet, setDataGet] = useState([]);
+  const [dataReserva, setDataReserva] = useState([]);
+  const [dataSolicitacao, setDataSolicitacao] = useState([]);
 
   useEffect(() => {
-    async function getInfo() {
-      const response = await fetch(url + "/", {
+    async function getSolicitacao(id: string) {
+      const response = await fetch(url + `/listaSolicitacao/${id}`, {
         method: "GET",
-        body: JSON.stringify(dataGet),
         headers: { "Content-Type": "application/json" },
       });
 
       if (response.ok) {
         const data = await response.json();
-        setDataGet(data);
+        setDataSolicitacao(data);
         console.log(url, data);
       }
     }
 
-    getInfo();
-  }, [dataGet]);
+    getSolicitacao(userContext.user.saram);
+  }, [dataSolicitacao]);
+
+  useEffect(() => {
+    async function getReserva(id: string) {
+      const response = await fetch(url + `/listaReserva/${id}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setDataReserva(data);
+        console.log(url, data);
+      }
+    }
+
+    getReserva(userContext.user.saram);
+  }, [dataReserva]);
 
   return (
     <>
@@ -42,33 +58,56 @@ const ReservasForm: any = async () => {
         }}
         subheader={<li />}
       >
-        {dataGet?.map((item: any) => (
-          <li key={`section-${item.id}`}>
-            {item.saram === userContext.user.saram ? (
-              <ul>
-                <Card sx={{ maxWidth: 330, marginBottom: 5, marginTop: 2 }}>
-                  <CardMedia
-                    sx={{ height: 140 }}
-                    image="https://s2-autoesporte.glbimg.com/OkOTAbm8c0hcBorNJK_n5VVN-3g=/0x0:1200x875/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2021/6/a/sh5QGuQUWsns9zfrqYGQ/fiat-doblo-50.jpg"
-                    title="carro"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {item.nome}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {item.status}
-                    </Typography>
-                  </CardContent>
-                  {/* <CardActions>
-                  <Button size="small">Aprovar</Button>
-                  <Button size="small">Solicitar</Button>
-                </CardActions> */}
-                </Card>
-              </ul>
-            ) : null}
-          </li>
-        ))}
+        <ul>
+          {dataSolicitacao?.map((item: any) => (
+            <Card sx={{ maxWidth: 330, marginBottom: 5, marginTop: 2 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://s2-autoesporte.glbimg.com/OkOTAbm8c0hcBorNJK_n5VVN-3g=/0x0:1200x875/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2021/6/a/sh5QGuQUWsns9zfrqYGQ/fiat-doblo-50.jpg"
+                title="carro"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.viatura}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {`
+                  Status da Solicitação: ${item.statusSolicitacao}
+                  Motivo: ${item.motivo}
+                  Destino: ${item.destino}
+                  Viatura: ${item.viatura}
+                  Placa: ${item.placa}
+                  `}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </ul>
+        <ul>
+          {dataReserva?.map((item: any) => (
+            <Card sx={{ maxWidth: 330, marginBottom: 5, marginTop: 2 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://s2-autoesporte.glbimg.com/OkOTAbm8c0hcBorNJK_n5VVN-3g=/0x0:1200x875/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2021/6/a/sh5QGuQUWsns9zfrqYGQ/fiat-doblo-50.jpg"
+                title="carro"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.viatura}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {`
+                  Status da Solicitação: ${item.statusSolicitacao}
+                  Motivo: ${item.motivo}
+                  Destino: ${item.destino}
+                  Viatura: ${item.viatura}
+                  Placa: ${item.placa}
+                  `}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </ul>
       </List>
     </>
   );
