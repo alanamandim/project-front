8d1289b20e42ef2e05db33860736531aef3063a4
Anderson Marlon <anderson18.marlon@gmail.com>
2023-11-01 11:54:25 -0300

    Fixed: ReservasVeiculo

--- src/pages/driver/reservasVeiculo/index.tsx
+++ src/pages/driver/reservasVeiculo/index.tsx
@@ -9,22 +9,22 @@ const ReservasVeiculoForm = () => {
 
   return (
     <>
-      {user.motorista === true ? (
-        <Grid
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          minHeight="80vh"
-        >
-          <Typography variant="h3" mt={3} mb={3} alignItems="center">
-            Reservation List
-          </Typography>
-          <ReservasForm />
-        </Grid>
-      ) : (
+      {/* {user.motorista === true ? ( */}
+      <Grid
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="80vh"
+      >
+        <Typography variant="h3" mt={3} mb={3} alignItems="center">
+          Reservation List
+        </Typography>
+        <ReservasForm />
+      </Grid>
+      {/* ) : (
         toast.error("Ops! Você não está autenticado como motorista, faça o login!")
-      )}
+      )} */}
     </>
   );
 };
