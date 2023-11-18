d604d457bbc068788eebf2b57713e21561f30fea
Beatriz Mandim <beatrizjuliomandim@gmail.com>
2023-11-18 17:11:55 -0300

    teste

--- src/pages/sender/components/Relatorio.tsx
+++ src/pages/sender/components/Relatorio.tsx
@@ -7,7 +7,7 @@ import { AuthContext } from "../../../context/AuthContext";
 const Relatorio = () => {
   const [selectedVehicle, setSelectedVehicle] = useState<string>("");
   const [selectedVehicle2, setSelectedVehicle2] = useState<string>("");
-  const { setRelatorio } = useContext(AuthContext);
+  const { relatorio, setRelatorio } = useContext(AuthContext);
 
   // const handleSubmit = (event: React.FormEvent) => {
   //   event.preventDefault();
@@ -36,7 +36,6 @@ const Relatorio = () => {
       });
       const data = await response.json();
       setRelatorio(data);
-      window.location.href = "/sender-relatorio";
     } else if (response.status === 401) {
       toast.error("Ops! Algo está incorreto.");
     } else {
@@ -46,7 +45,31 @@ const Relatorio = () => {
     }
   }
 
-  return (
+  return relatorio ? (
+    <>
+      <h1>Relatório</h1>
+      {relatorio.map((item: any) => {
+        <>
+          <h3>Relatório {item.idSolicitacao}</h3>
+          <textarea name="relatorio" id="relatorio">
+            Id da Solicitação: {item.idSolicitacao} &#10; Motivo da Missão:{" "}
+            {item.motivoMissao} &#10; Destino: {item.destino} &#10; Viatura:{" "}
+            {item.viatura} &#10; Motorista: {item.motorista} &#10; Aprovador:{" "}
+            {item.aprovador} &#10; Id do Registro: {item.idRegistro} &#10; KM
+            Inicial: {item.kmInicial} &#10; Status da Missão:{" "}
+            {item.statusMissao} &#10; KM Final: {item.kmFinal} &#10; Data/Hora
+            da Saída: {item.dataHrSaida} &#10; Data/Hora da Chegada:{" "}
+            {item.dataHrChegada} &#10; Retorno do motorista:{" "}
+            {item.motoristaRetorno} &#10; Óleo: {item.oleo} &#10; Pneu:{" "}
+            {item.pneu} &#10; Água do Radiador: {item.aguaRadiador} &#10;
+            Amassado: {item.amassado} &#10; Arranhado: {item.aranhado} &#10;
+            Tanque: {item.tanque} &#10; Observação: {item.observacao} &#10;
+            Aprovador de Retorno: {item.aprovadorRetorno} &#10;
+          </textarea>
+        </>;
+      })}
+    </>
+  ) : (
     <>
       <Grid item mb={3}>
         <form>
