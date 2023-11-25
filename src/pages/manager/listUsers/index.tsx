24b58ae4c7a835f6563a1cbd9f1d6feb1dcec901
Beatriz Mandim <beatrizjuliomandim@gmail.com>
2023-11-25 19:22:56 -0300

    ajustes

--- src/pages/manager/listUsers/index.tsx
+++ src/pages/manager/listUsers/index.tsx
@@ -9,25 +9,25 @@ const ListUsersPage = () => {
 
   return (
     <>
-      {/* {user.gestor === true ? ( */}
-      <Grid
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="80vh"
-      >
-        <Typography variant="h3" mt={3} mb={3} alignItems="center">
-          List all Users
-        </Typography>
-        <Typography variant="h4" mt={3} alignItems="center">
-          Todos os pedidos dos usuários
-        </Typography>
-        <ListUsersCard />
-      </Grid>
-      {/* ) : (
+      {user.gestor === true ? (
+        <Grid
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="80vh"
+        >
+          <Typography variant="h3" mt={3} mb={3} alignItems="center">
+            List all Users
+          </Typography>
+          <Typography variant="h4" mt={3} alignItems="center">
+            Todos os pedidos dos usuários
+          </Typography>
+          <ListUsersCard />
+        </Grid>
+      ) : (
         toast.error("Ops! Algo deu errado, saia e entre novamente!")
-      )} */}
+      )}
     </>
   );
 };
